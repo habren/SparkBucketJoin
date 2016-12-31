4ef44d88d33ca4104043876149ab81f4af00844c
Jason Guo <jason.guo.vip@gmail.com>
2016-12-31 09:42:17 +0800

    Update TupleComparator

--- src/main/java/com/jasongj/spark/utils/TupleComparator.java
+++ src/main/java/com/jasongj/spark/utils/TupleComparator.java
@@ -54,21 +54,23 @@ public class TupleComparator implements Comparator<Tuple> {
                 return 1;
             } else {
                 Preconditions.checkArgument(field1.getClass().getCanonicalName().equals(field2.getClass().getCanonicalName()), "Key set for tuple1 and tuple2 should have the same type");
-                String type = field1.getClass().getSimpleName();
+                String type = field1.getClass().getSimpleName().toLowerCase();
                 String str1 = field1.toString();
                 String str2 = field2.toString();
                 switch(type) {
-                    case "String": return String.valueOf(str1).compareTo(String.valueOf(str2));
-                    case "BigDecimal": return BigDecimal.valueOf(Long.valueOf(str1)).compareTo(BigDecimal.valueOf(Long.valueOf(str2)));
-                    case "Integer": return Integer.valueOf(str1).compareTo(Integer.valueOf(str2));
-                    case "Long": return Long.valueOf(str1).compareTo(Long.valueOf(str2));
-                    case "Double": return Double.valueOf(str1).compareTo(Double.valueOf(str2));
-                    case "Float": return Float.valueOf(str1).compareTo(Float.valueOf(str2));
-                    case "Boolean": return Boolean.valueOf(str1).compareTo(Boolean.valueOf(str2));
-                    case "Date": return Date.valueOf(str1).compareTo(Date.valueOf(str2));
-                    case "Timestamp": return Timestamp.valueOf(str1).compareTo(Timestamp.valueOf(str2));
-                    case "Short": return Short.valueOf(str1).compareTo(Short.valueOf(str2));
-                    case "Byte": return Byte.valueOf(str1).compareTo(Byte.valueOf(str2));
+                    case "string": return String.valueOf(str1).compareTo(String.valueOf(str2));
+                    case "bigdecimal": return BigDecimal.valueOf(Long.valueOf(str1)).compareTo(BigDecimal.valueOf(Long.valueOf(str2)));
+                    case "integer": return Integer.valueOf(str1).compareTo(Integer.valueOf(str2));
+                    case "long": return Long.valueOf(str1).compareTo(Long.valueOf(str2));
+                    case "double": return Double.valueOf(str1).compareTo(Double.valueOf(str2));
+                    case "float": return Float.valueOf(str1).compareTo(Float.valueOf(str2));
+                    case "boolean": return Boolean.valueOf(str1).compareTo(Boolean.valueOf(str2));
+                    case "date": return Date.valueOf(str1).compareTo(Date.valueOf(str2));
+                    case "timestamp": return Timestamp.valueOf(str1).compareTo(Timestamp.valueOf(str2));
+                    case "short":
+                    case "smallint": return Short.valueOf(str1).compareTo(Short.valueOf(str2));
+                    case "tinyint":
+                    case "byte": return Byte.valueOf(str1).compareTo(Byte.valueOf(str2));
                     default: throw new RuntimeException("Unsupported data type: " + type);
                 }
             }
