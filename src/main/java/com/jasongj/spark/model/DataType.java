4cdb705085e38ed9c678d466c45fbd5ba70c6e1d
Jason Guo <jason.guo.vip@gmail.com>
2017-01-20 20:37:25 +0800

    Add writer class for each DataType

--- src/main/java/com/jasongj/spark/model/DataType.java
+++ src/main/java/com/jasongj/spark/model/DataType.java
@@ -3,6 +3,9 @@ package com.jasongj.spark.model;
 import com.jasongj.spark.reader.BucketReaderIterator;
 import com.jasongj.spark.reader.ParquetBucketReaderIterator;
 import com.jasongj.spark.reader.TextBucketReaderIterator;
+import com.jasongj.spark.writer.ParquetTupleWriter;
+import com.jasongj.spark.writer.TextTupleWriter;
+import com.jasongj.spark.writer.TupleWriter;
 import lombok.Getter;
 
 import java.util.Arrays;
@@ -12,15 +15,22 @@ import java.util.List;
  * Created by Jason Guo (jason.guo.vip@gmail.com)
  */
 public enum DataType {
-    TEXT(TextBucketReaderIterator.class, Arrays.asList("org.apache.hadoop.mapred.TextInputFormat", "org.apache.hadoop.mapreduce.lib.input.TextInputFormat")),
-    PARQUET(ParquetBucketReaderIterator.class, Arrays.asList("org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat", "org.apache.parquet.hadoop.ParquetInputFormat"));
+    TEXT(TextBucketReaderIterator.class, TextTupleWriter.class, Arrays.asList("org.apache.hadoop.mapred.TextInputFormat",
+            "org.apache.hadoop.mapreduce.lib.input.TextInputFormat", "org.apache.hadoop.mapred.TextOutputFormat",
+            "org.apache.hadoop.mapreduce.lib.output.TextOutputFormat")),
+    PARQUET(ParquetBucketReaderIterator.class, ParquetTupleWriter.class, Arrays.asList("org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat",
+            "org.apache.parquet.hadoop.ParquetInputFormat", "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat",
+            "org.apache.parquet.hadoop.ParquetOutputFormat"));
 
     @Getter
-    private Class<? extends BucketReaderIterator> readClass;
+    private Class<? extends BucketReaderIterator> readerClass;
+    @Getter
+    private Class<? extends TupleWriter> writerClass;
     @Getter private List<String> inputFormats;
 
-    DataType(Class<? extends BucketReaderIterator> readClass, List<String> inputFormats) {
-        this.readClass = readClass;
+    DataType(Class<? extends BucketReaderIterator> readerClass, Class<? extends TupleWriter> writerClass, List<String> inputFormats) {
+        this.readerClass = readerClass;
+        this.writerClass = writerClass;
         this.inputFormats = inputFormats;
     }
 }
