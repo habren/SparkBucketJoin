6926409ed60f7231a7501b10c8b74681c31db781
Jason Guo <jason.guo.vip@gmail.com>
2017-01-27 21:13:43 +0800

    Refactor TupleWriter -- remove DataOutputStream from constructor and member variable

--- src/main/java/com/jasongj/spark/writer/TupleWriter.java
+++ src/main/java/com/jasongj/spark/writer/TupleWriter.java
@@ -2,9 +2,9 @@ package com.jasongj.spark.writer;
 
 import com.jasongj.spark.model.TableMetaData;
 import com.jasongj.spark.model.Tuple;
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hive.metastore.api.TableMeta;
 
 import java.io.DataOutputStream;
 
@@ -14,18 +14,16 @@ import java.io.DataOutputStream;
 public abstract class TupleWriter {
     protected Path path;
     protected Configuration hadoopConfiguration;
-    protected DataOutputStream dataOutputStream;
     protected TableMetaData outputTableMetaData;
 
-    public TupleWriter(DataOutputStream dataOutputStream, Configuration hadoopConfiguration, Path path, TableMetaData outputTableMetaData) {
-        this.dataOutputStream = dataOutputStream;
+    public TupleWriter(Configuration hadoopConfiguration, Path path, TableMetaData outputTableMetaData) {
         this.hadoopConfiguration = hadoopConfiguration;
         this.path = path;
         this.outputTableMetaData = outputTableMetaData;
     }
 
     public boolean init() {
-        return dataOutputStream != null && hadoopConfiguration != null && path != null && outputTableMetaData != null;
+        return hadoopConfiguration != null && path != null && outputTableMetaData != null;
     }
     public abstract void write(Tuple tuple);
     public abstract void close();
